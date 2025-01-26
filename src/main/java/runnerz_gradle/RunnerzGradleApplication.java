ad35b3ae530a9c82f9ba74939ddd8eef36221fe5
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-26 15:09:33 +0330

    Add CommandLineRunner to log run details on application startup

--- src/main/java/runnerz_gradle/RunnerzGradleApplication.java
+++ src/main/java/runnerz_gradle/RunnerzGradleApplication.java
@@ -1,19 +1,35 @@
 package runnerz_gradle;
 
+import java.time.LocalDateTime;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import runnerz_gradle.run.Location;
+import runnerz_gradle.run.Run;
 
 @SpringBootApplication
 public class RunnerzGradleApplication {
 
 	private static final org.slf4j.Logger log = LoggerFactory.getLogger(RunnerzGradleApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzGradleApplication.class, args);
-		log.info("Application is running successfully");
-		
-		
+
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+
+			Run run = new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5,
+					Location.Outdoor);
+
+			log.info("Run: " + run);
+		};
 	}
 
 }
