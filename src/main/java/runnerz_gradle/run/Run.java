f14db7380ed5fb1c5c0706917b7709d2ccb8c01c
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-26 15:59:13 +0330

    Rename location field in Run record to avoid naming conflict with Location enum

--- src/main/java/runnerz_gradle/run/Run.java
+++ src/main/java/runnerz_gradle/run/Run.java
@@ -2,6 +2,6 @@ package runnerz_gradle.run;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles, Location location) {
+public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles, Location Location) {
 
 }
