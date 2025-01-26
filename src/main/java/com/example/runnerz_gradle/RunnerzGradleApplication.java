440119fac6a81355d3f793273c7c1fad7020f698
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-26 10:59:13 +0330

    Initialize Gradle project with basic configuration and application structure

---
+++ src/main/java/com/example/runnerz_gradle/RunnerzGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.runnerz_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerzGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerzGradleApplication.class, args);
+	}
+
+}
