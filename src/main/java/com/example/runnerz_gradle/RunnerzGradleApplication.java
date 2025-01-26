cf3c5fe85faf9b40f44a30cc22798e7b3d30aafe
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-26 11:14:42 +0330

    s

--- src/main/java/com/example/runnerz_gradle/RunnerzGradleApplication.java
+++ src/main/java/com/example/runnerz_gradle/RunnerzGradleApplication.java
@@ -3,11 +3,15 @@ package com.example.runnerz_gradle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import foo.WelcomeMessage;
+
 @SpringBootApplication
 public class RunnerzGradleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzGradleApplication.class, args);
+		var welcomeMessage = new WelcomeMessage();
+		System.out.println(welcomeMessage.getWelcomeMessage());
 	}
 
 }
