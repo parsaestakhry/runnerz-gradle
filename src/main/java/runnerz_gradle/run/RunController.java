fdf10e5883668b4e0f1d670fadf0228dde6e64c6
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-26 16:15:54 +0330

    Add API endpoints to RunController for retrieving all runs and a specific run by ID

--- src/main/java/runnerz_gradle/run/RunController.java
+++ src/main/java/runnerz_gradle/run/RunController.java
@@ -1,16 +1,32 @@
 package runnerz_gradle.run;
 
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
-
-
-    @GetMapping("/hello")
-    String home() {
-        return "Hello World!";
+    private final RunRepository runRepository;
+    
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
     }
+    
+    @GetMapping("")
+    List<Run> getRuns() {
+        return runRepository.findAll();
+    }
+
+    @GetMapping("/1")
+    Run findById() {
+        return runRepository.findById(1);
+    }
+    
+
 }
