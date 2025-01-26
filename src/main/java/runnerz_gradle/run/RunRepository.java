329706b7f3d39725b7cf778fb990510ac93bdd64
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-26 16:16:06 +0330

    Add Spring @Repository annotation and refactor RunRepository methods for clarity

--- src/main/java/runnerz_gradle/run/RunRepository.java
+++ src/main/java/runnerz_gradle/run/RunRepository.java
@@ -4,12 +4,15 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import jakarta.annotation.PostConstruct;
 
+@Repository
 public class RunRepository {
     private List<Run> runs = new ArrayList<Run>();
 
-    List<Run> getRuns() {
+    List<Run> findAll() {
         return runs;
     }
 
@@ -17,5 +20,11 @@ public class RunRepository {
     private void init() {
         runs.add(new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5, Location.Outdoor));
     }
+
+    Run findById(Integer i) {
+        return runs.stream().filter(run -> run.id().equals(i)).findFirst().orElse(null);
+    }
+
+    
 
 }
