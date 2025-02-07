3fc9cf5fbe47a5ee88c5b707d3f782d6a6061abe
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-07 12:01:00 +0330

    Add error handling to RunController findById method for non-existent runs

--- src/main/java/runnerz_gradle/run/RunController.java
+++ src/main/java/runnerz_gradle/run/RunController.java
@@ -2,11 +2,14 @@ package runnerz_gradle.run;
 
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 
@@ -27,7 +30,12 @@ public class RunController {
 
     @GetMapping("/{id}")
     Run findById(@PathVariable("id") Integer id) {
-        return runRepository.findById(id);
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+        } else {
+            return run.get();
+        }
     }
     
     
