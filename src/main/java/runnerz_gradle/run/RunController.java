02d875905faaa084c06919a9b4f581d0cc5d98ce
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-08 19:53:03 +0330

    Add updateRun and deleteRun endpoints to RunController for run management

--- src/main/java/runnerz_gradle/run/RunController.java
+++ src/main/java/runnerz_gradle/run/RunController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -50,10 +52,21 @@ public class RunController {
     }
 
 
-    
+    // put
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void updateRun(@RequestBody Run run, @PathVariable("id") Integer id) {
+        runRepository.updateRun(run, id);
+    }
 
 
-    
+    // delete 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteRun(@PathVariable("id") Integer id) {
+        runRepository.deleteRun(id);
+    }
     
 
 }
