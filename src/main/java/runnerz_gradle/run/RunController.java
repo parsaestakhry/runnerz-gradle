562e1f9eec2f7f7a3c361913083d4e38e5982c45
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-08 19:42:41 +0330

    Add createRun method to RunController for creating new runs

--- src/main/java/runnerz_gradle/run/RunController.java
+++ src/main/java/runnerz_gradle/run/RunController.java
@@ -7,7 +7,10 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -39,6 +42,14 @@ public class RunController {
     }
     
     
+    // post
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    void createRun(@RequestBody Run run) {
+        runRepository.createRun(run);
+    }
+
+
     
     
 
