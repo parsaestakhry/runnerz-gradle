326e5fe8cebadd64c006a9d76e3baaf0f29b086b
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-08 20:15:36 +0330

    Add validation to Run record to ensure completed date is after start date

--- src/main/java/runnerz_gradle/run/Run.java
+++ src/main/java/runnerz_gradle/run/Run.java
@@ -2,6 +2,17 @@ package runnerz_gradle.run;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles, Location Location) {
+public record Run(Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        Integer miles,
+        Location Location) {
+
+    public Run {
+        if (!completedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Completed date must be after start date");
+        }
+    }
 
 }
